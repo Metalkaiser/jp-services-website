64b36e4727daa5ae868471338974c142ce8b236b
MetalKaiser <metalkaiserpolanco@gmail.com>
2025-05-05 17:18:19 -0400

    Seccion servicios

--- src/app/utils/info.tsx
+++ src/app/utils/info.tsx
@@ -34,3 +34,42 @@ export const topCta = {
   topCtaBtnText: "Haz click aquí",
   topCtaBtnhref: "#services"
 }
+
+export const servicesSection = {
+  title: "Conoce nuestros servicios",
+  description: `En ${websiteName} ofrecemos una amplia gama de servicios para ayudar a tu negocio a crecer y prosperar en el mundo digital. Desde el desarrollo de aplicaciones web personalizadas hasta la creación de tiendas en línea, tenemos la solución perfecta para ti.`,
+}
+
+export const services = [
+  {
+    title: "Desarrollo de aplicaciones web",
+    description: "Creamos aplicaciones web personalizadas que se adaptan a las necesidades específicas de tu negocio, incluyendo sistemas de gestión, tiendas en línea y más.",
+    icon: "https://cdn-icons-png.flaticon.com/512/8414/8414759.png"
+  },
+  {
+    title: "Diseño web",
+    description: "Diseñamos sitios web atractivos y funcionales que reflejan la identidad de tu marca y mejoran la experiencia del usuario.",
+    icon: "https://cdn-icons-png.flaticon.com/512/8820/8820192.png"
+  },
+  {
+    title: "SEO y marketing digital",
+    description: "Ayudamos a mejorar la visibilidad de tu negocio en línea a través de estrategias de SEO y marketing digital efectivas.",
+    icon: "https://cdn-icons-png.flaticon.com/512/4693/4693249.png"
+  },
+  {
+    title: "Soporte y mantenimiento",
+    description: "Ofrecemos soporte técnico y mantenimiento continuo para asegurarnos de que tu sitio web o aplicación funcione sin problemas.",
+    icon: "https://cdn-icons-png.flaticon.com/512/8759/8759045.png"
+  },
+  {
+    title: "Consultoría tecnológica",
+    description: "Brindamos asesoría en tecnología y transformación digital para ayudarte a tomar decisiones informadas sobre el futuro de tu negocio.",
+    icon: "https://cdn-icons-png.flaticon.com/512/8185/8185258.png"
+  },
+  {
+    title: "Integraciones y automatización",
+    description: "Integramos diferentes sistemas y herramientas para optimizar tus procesos y mejorar la eficiencia de tu negocio.",
+    icon: "https://cdn-icons-png.flaticon.com/512/8438/8438906.png"
+  },
+  
+]
